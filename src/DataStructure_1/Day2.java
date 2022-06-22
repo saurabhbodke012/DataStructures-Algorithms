b13c564bad243c35c31fd5e114a01b8c1d348576
Saurabh Bodke <saurabhbodke@Saurabhs-MacBook-Pro.local>
2022-06-22 18:16:45 -0400

    dsa-1 day2 problem 1

--- src/DataStructure_1/Day2.java
+++ src/DataStructure_1/Day2.java
@@ -1,4 +1,26 @@
 package DataStructure_1;
 
+import java.util.HashMap;
+
 public class Day2 {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] res= new int[2];
+
+        for(int i=0; i<nums.length;i++){
+
+            if(map.containsKey(target -  nums[i])){
+                res[0]= i;
+                res[1]= map.get(target- nums[i]);
+            }else{
+                map.put(nums[i], i);
+            }
+
+        }
+
+        return res;
+
+    }
 }
