ea6ad43faa4ab1e4af19281461d0c7c7161ff4ae
Saurabh Bodke <saurabhbodke012@gmail.com>
2022-06-23 20:29:26 -0400

    problem 2

--- src/DataStructure_1/Day2.java
+++ src/DataStructure_1/Day2.java
@@ -25,12 +25,23 @@ public class Day2 {
         return res;
     }
 
-    public int maxDepth(TreeNode root) {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
 
-        if(root== null){
-            return 0;
+        int index1 = m - 1;
+        int index2 = n - 1;
+
+
+        for (int i = nums1.length-1; i >= 0; i--) {
+            if (index2 < 0) {
+                break;
+            }
+            if (index1 >= 0 && nums1[index1] > nums2[index2]) {
+                nums1[i] = nums1[index1];
+                index1--;
+            } else {
+                nums1[i] = nums2[index2];
+                index2--;
+            }
         }
-
-        return Math.max(maxDepth(root.left),maxDepth(root.right)) + 1;
     }
 }
--- src/Recursion1/June22nd.java
+++ src/Recursion1/June22nd.java
@@ -46,4 +46,13 @@ public class June22nd {
 
     }
 
+    public int maxDepth(TreeNode root) {
+
+        if(root== null){
+            return 0;
+        }
+
+        return Math.max(maxDepth(root.left),maxDepth(root.right)) + 1;
+    }
+
 }
