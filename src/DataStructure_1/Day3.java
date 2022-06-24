2bc014f28f27a227ad40cf470a1bdd2352ed9194
Saurabh Bodke <saurabhbodke012@gmail.com>
2022-06-24 14:37:31 -0400

    day 3 dsa-1 study plan

--- src/DataStructure_1/Day3.java
+++ src/DataStructure_1/Day3.java
@@ -1,5 +1,8 @@
 package DataStructure_1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Day3 {
 
     public int maxProfit(int[] prices) {
@@ -24,4 +27,35 @@ public class Day3 {
             return profit;
         }
     }
+
+
+    public int[] intersect(int[] nums1, int[] nums2) {
+
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        int i=0, j=0;
+
+        ArrayList<Integer> result= new ArrayList<Integer>();
+        while (i< nums1.length && j< nums2.length){
+            if (nums1[i] == nums2[j]){
+                result.add(nums1[i]);
+                i++;
+                j++;
+            }else if (nums1[i] < nums2[j]){
+                i++;
+            }else {
+                j++;
+            }
+        }
+
+        int[] arr = new int[result.size()];
+        for (int k =0; k<result.size();k++){
+            arr[k] = result.get(k);
+        }
+
+        return arr;
+
+    }
+
 }
