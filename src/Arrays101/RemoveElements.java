34d8b122b87fe3321e9ac5a5b6681325c567ba3b
Saurabh Bodke <saurabhbodke012@gmail.com>
2022-07-05 21:55:48 -0400

    remove elements

---
+++ src/Arrays101/RemoveElements.java
@@ -0,0 +1,23 @@
+package Arrays101;
+
+public class RemoveElements {
+    public int removeElement(int[] nums, int val) {
+
+        int start = 0;
+        int end = nums.length;
+
+        while(start < end){
+
+            if(nums[start] == val){
+                nums[start]= nums[end -1];
+                end--;
+            }else{
+                start++;
+            }
+
+        }
+
+        return end;
+    }
+
+}
