b8f11f0c7f5ee9a28a128b5621003c27ec1a308a
Saurabh Bodke <saurabhbodke012@gmail.com>
2022-07-08 12:15:58 -0400

    movezeros

---
+++ src/Arrays101/MoveZeros.java
@@ -0,0 +1,17 @@
+package Arrays101;
+
+public class MoveZeros {
+    public void moveZeroes(int[] nums) {
+
+        int i=0;
+        for(int j=0; j<nums.length; j++){
+            if(nums[j] != 0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+            }
+        }
+    }
+
+}
