84f23c9d113639ac88161d7adcb203af7742d7cb
Saurabh Bodke <saurabhbodke012@gmail.com>
2022-07-08 15:17:53 -0400

    sort array by parity

---
+++ src/Arrays101/SortByParity.java
@@ -0,0 +1,23 @@
+package Arrays101;
+
+public class SortByParity {
+
+    public int[] sortArrayByParity(int[] nums) {
+        int i=0;
+
+        for(int j=0; j<nums.length; j++){
+
+            if( nums[j] % 2 == 0 ){
+                int temp= nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+            }
+
+        }
+
+        return nums;
+    }
+
+
+}
