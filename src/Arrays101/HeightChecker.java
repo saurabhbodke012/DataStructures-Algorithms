87c6ae7e3b57967edc177ac71471ca59a7e3da07
Saurabh Bodke <saurabhbodke012@gmail.com>
2022-07-08 16:12:56 -0400

    height checker

---
+++ src/Arrays101/HeightChecker.java
@@ -0,0 +1,25 @@
+package Arrays101;
+
+public class HeightChecker {
+    public int heightChecker(int[] heights) {
+        int[] arr= new int[heights.length];
+
+        for(int i=0; i<heights.length; i++){
+            arr[i] = heights[i];
+        }
+
+        Arrays.sort(arr);
+        int count=0;
+        for(int i=0; i<heights.length; i++){
+
+            if(arr[i] != heights[i]){
+                count ++;
+            }
+
+        }
+
+        return count;
+
+    }
+
+}
