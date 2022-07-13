4e542a9b3f7c5d37d9966ab106d547acad4fac12
Saurabh Bodke <saurabhbodke012@gmail.com>
2022-07-12 20:15:23 -0400

    odd even list

---
+++ src/LinkedList/OddEvenList.java
@@ -0,0 +1,25 @@
+package LinkedList;
+
+public class OddEvenList {
+
+    public ListNode oddEvenList(ListNode head) {
+
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+
+        while( even != null && even.next != null){
+
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+
+        }
+
+        odd.next = evenHead;
+        return head;
+
+    }
+
+}
